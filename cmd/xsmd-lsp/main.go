59602bccb4dc5c9f29e359ca4d6c587718c12d96
gcgbarbosa <gcgbarbosa@gmail.com>
2026-06-29 13:01:40 +0200

    feat: add logging to file in main.go for better diagnostics

--- cmd/xsmd-lsp/main.go
+++ cmd/xsmd-lsp/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"time"
 
 	"github.com/RaquerLabs/xsmd/internal/lsp"
 	"github.com/RaquerLabs/xsmd/internal/state"
@@ -14,4 +17,10 @@ func main() {
 
 	s := server.NewServer(handler, "xsmd-lsp", false)
 	log.Fatal(s.RunStdio())
+}
+
+func logToFile(msg string) {
+	f, _ := os.OpenFile("xsmd.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	defer f.Close()
+	f.WriteString(fmt.Sprintf("[%s] %s\n", time.Now().Format("15:04:05"), msg))
 }
